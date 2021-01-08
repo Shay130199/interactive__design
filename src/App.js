923156e3510dc9704e2d1eeb3df1890c60729619
Shay-Lynn Dudhia <55833100+Shay130199@users.noreply.github.com>
2021-01-08 06:49:13 +0000

    Message (Card)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React,{ useState } from 'react';
-import Modal from 'react-modal'
+import React from "react"; 
 import './App.css';
+import Tweet from './Tweet';
 
-Modal.setAppElement('#root')
+
 function App() {
-  const [modalIsOpen, setmodalIsOpen] = useState(false)
   return (
-    <div className='App'>
-      <button onClick={() => setmodalIsOpen(true) }>Open Me!</button>
-      <Modal isOpen={modalIsOpen} shouldonRequestClose={()=>setmodalIsOpen(false)}>
-        <h2>
-          WOULD YOU LIKE TO SPEAK TO A SPECIALIST....
-        </h2>
-        <p>Well, your at the right place! At BoostMe we provide all our costumers the best treatment they deserve! </p>
-        <div>
-          <button onClick={( ) => setmodalIsOpen(false) }>Close</button>
-        </div>
-      </Modal>
+    <div className= "app">
+      <Tweet />
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/Tweet.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function Tweet() {
+    return (
+        <div className="tweet">
+            <h3> Feel abit Under the weather? </h3>
+            <h3>We are always here to talk....</h3>
+            <ul>Read More</ul>
+        </div>
+
+    );
+}
+
+export default Tweet;
