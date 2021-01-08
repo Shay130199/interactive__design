4a0fc5f77ea469c949214229a2efdbbd276fb0ad
Shay-Lynn Dudhia <55833100+Shay130199@users.noreply.github.com>
2021-01-08 05:27:58 +0000

    Modal Notification
    Modal Notification

--- src/App.js
+++ src/App.js
@@ -1,44 +1,24 @@
-import React, { Component } from "react";
-import NarBar from "./components/layout/Nav.Bar";
-import logo from "../src/assets/logo.png";
-import title from "../src/assets/boostme.png";
-import SideNavBar from "./components/layout/SideNavBar";
-import BackDrop from "./components/layout/Backdrop";
+import React,{ useState } from 'react';
+import Modal from 'react-modal'
+import './App.css';
 
-class App extends Component {
-    state = {
-        sideBarOpen: false
-    }
-
-    handleOpen = () => {
-        this.setState({
-            sideBarOpen: true
-        })
-    }
-
-    handleClose = () => {
-        this.setState({
-            sideBarOpen: false
-        })
-    }
-    render() {
-        return (
-            <div>
-                <NarBar open={this.handleOpen} />
-                {this.state.sideBarOpen ? (<div><SideNavBar close={this.handleClose} />
-                    <BackDrop /> </div>) : null}
-                <div style={{ marginTop: "60px", marginLeft: "9px" }}>
-
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <p>
-                            <img src={title} className="Title"></img>
-                        </p>
-                    </header>
-                </div>
-            </div>
-        );
-    }
+Modal.setAppElement('#root')
+function App() {
+  const [modalIsOpen, setmodalIsOpen] = useState(false)
+  return (
+    <div className='App'>
+      <button onClick={() => setmodalIsOpen(true) }>Open Me!</button>
+      <Modal isOpen={modalIsOpen} shouldonRequestClose={()=>setmodalIsOpen(false)}>
+        <h2>
+          WOULD YOU LIKE TO SPEAK TO A SPECIALIST....
+        </h2>
+        <p>Well, your at the right place! At BoostMe we provide all our costumers the best treatment they deserve! </p>
+        <div>
+          <button onClick={( ) => setmodalIsOpen(false) }>Close</button>
+        </div>
+      </Modal>
+    </div>
+  )
 }
 
 export default App;
